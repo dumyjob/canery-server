3d0f604e7c5dfdb0273f41963b034c68e58e3533
xuanfeng.shen <xuanfeng.shen@atrenew.com>
2025-05-15 10:25:49 +0800

    create project

---
+++ src/test/java/com/github/shen/canery_sever/CanerySeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shen.canery_sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanerySeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
