ab88635e90f8a02029bf1a23ebdcda51cc36ca22
shenxf <shenxf_T_1000@163.com>
2025-05-18 14:10:30 +0800

    一键部署发布系统 python部署任务ros

--- python-celary/aliyun/ros.py
+++ python-celary/aliyun/ros.py
@@ -1,6 +1,7 @@
 # aliyun/ros.py
 from aliyunsdkcore.client import AcsClient
-from aliyunsdkros.request.v20190910 import CreateStackRequest, DescribeStacksRequest
+from aliyunsdkros.request.v20190910 import CreateStackRequest, GetStackRequest
+
 
 class ROSClient:
     def __init__(self, access_key, secret_key,region=None):
@@ -13,7 +14,7 @@ class ROSClient:
         return response.get('StackId')
 
     def get_stack_status(self, stack_id):
-        request = DescribeStacksRequest.DescribeStacksRequest()
+        request = GetStackRequest.GetStackRequest()
         request.set_StackId(stack_id)
         response = self.client.do_action_with_exception(request)
         return response.get('Stacks')[0].get('Status')
